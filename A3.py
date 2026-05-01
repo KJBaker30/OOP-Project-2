6a5798be31af446b8f25658f0731ae1c8eaefa20
Keira <k.baker30@icloud.com>
2026-05-01 07:59:15 +0100

    laptimes done

--- A3.py
+++ A3.py
@@ -2,69 +2,49 @@ a = 1.5
 b = 0.03
 
 
-class Tyre():                                                # there is a tyre
-    def __init__(self, degradation, performance):            # tyre has a degradation value and performance value 
-        self.degradation = degradation
-        self.deg = degradation                               # self.deg = self.degradation
+class Tyre():                                               
+    def __init__(self, degradation, performance):            
+        self.deg = degradation                               
         self.performance = performance
         
-        self.deg = 0.0                                       # tyre deg = 0.0
-        self.performance = 1.0                               # tyre performance = 1.0
+        self.deg = 0.0                                       
+        self.performance = 1.0                             
 
-    def updateDegradation(self):                             # update of tyre deg value     
-        degradation_increase = a + b*self.deg                # deg increase = 1.5 + 0.03(0)  -  base decrease in deg per lap = 1.5
-        self.deg = round(degradation_increase + self.deg, 3)
-        
+    def updateDegradation(self):                                  
+        degradation_increase = a + b*self.deg               
+        self.deg = degradation_increase + self.deg  
 
-    def updatePerformance(self):                             # update of tyre performance
-        performance_decrease = 1 - 0.003*self.deg - 0.00008*self.deg*self.deg   # performance decrease = 1 - deg value(0.03) - deg value ^2 (0.00008)
-        self.performance = round(performance_decrease, 5)
+    def updatePerformance(self):                            
+        performance_decrease = 1 - 0.003*self.deg - 0.00008*self.deg*self.deg   
+        self.performance = performance_decrease
 
-        
-        
-# class Track():                                               # There is a track
-    # def __init__(self, distance):                            # The track has a distance - not sure how to add measurements here (e.g. km or miles)
-    #     self.distance = distance  
-        # distance above sea level
-        # weather conditions
-        # no. of cars on track
-        # elevation
 
        
-
-class LapTime():                                             # there is a laptime 
-    def __init__(self, minutes, seconds, base_time):         # the laptime has minutes and seconds and a base lap time
-       self.minutes = minutes
-       self.seconds = seconds
-       self.base_time = base_time         
+class LapTime():                                              
+    def __init__(self, base_time):
+       self.base_time = base_time
+       base_time = 90
+       self.provisional_lap_time = 0.0       
        
-    def provisional_lap_time(self, base_time, minutes, seconds):        # there is a provisional lap time that has minutes and seconds
-           self.minutes = minutes
-           self.seconds = seconds
-           self.base_time = base_time
-           base_time = 1*minutes + 00*seconds
-           self.provisional_lap_time = base_time + self.provisional_lap_time*(Tyre.performance(Tyre.deg(self.provisional_lap_time)))
-# print('Provisional Lap Time', LapTime.provisional_lap_time)
-
+    def calcProvisionalLapTime(self, tyre):      
+        self.provisional_lap_time = self.base_time + self.provisional_lap_time*(1-tyre.performance) 
+        return self.provisional_lap_time
+    
+    def getProvisionalLapTime(self):
+        minutes = int(self.provisional_lap_time // 60)
+        seconds = int(self.provisional_lap_time % 60)
+        milliseconds = int((self.provisional_lap_time - (minutes*60 + seconds))*1000)
+        return f"{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
 
 
 
 tyre_one = Tyre('0.0', '1.0')
+laptime_manager = LapTime(90)
 
-tyre_one.updateDegradation()
-print('Tyre Degradation:', tyre_one.deg)
-tyre_one.updatePerformance()
-print('Performance Value:', ((tyre_one.performance)*100),'%')
-
-
-
-for lap in range(0,10):
+for lap in range(1,10):
     tyre_one.updateDegradation()
     tyre_one.updatePerformance()
-    print('Lap', lap, 'Tyre Degradation:', (tyre_one.deg))
-    print('Lap', lap, 'Performance Value:', ((tyre_one.performance)*100), '%')
+    laptime_manager.calcProvisionalLapTime(tyre_one)
+    print(f"Lap: {lap}  Tyre Degradation: {tyre_one.deg:.2f}%   Tyre Performance: {tyre_one.performance*100:.2f}%")
+    print(f"Provisional Lap Time: {laptime_manager.getProvisionalLapTime()}")
 
-
-
-
-       
